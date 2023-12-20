d7d5c2bfec7e8716cc881b12ec20a29f6b00a38d
Edgaras Austinskas <eaustinskas@gmail.com>
2023-12-20 17:52:07 +0200

    made some tasks

--- paskaita_4/index.js
+++ paskaita_4/index.js
@@ -2,32 +2,27 @@ const express = require('express');
 const app = express();
 const data = require('./data');
 
-// Route 1: Generic GET route that serves all the data
 app.get('/api/data', (req, res) => {
   res.json(data);
 });
 
-// Route 2: Dynamic GET route filtering by car make
 app.get('/api/data/cars/:make', (req, res) => {
   const make = req.params.make;
   const filteredData = data.filter(item => item.car === make);
   res.json(filteredData);
 });
 
-// Route 3: Dynamic GET route for user by ID
 app.get('/api/data/user/:id', (req, res) => {
   const userId = parseInt(req.params.id);
   const user = data.find(item => item.id === userId);
   res.json(user);
 });
 
-// Route 4: GET route for all emails
 app.get('/api/data/emails', (req, res) => {
   const emails = data.map(item => item.email);
   res.json(emails);
 });
 
-// Route 5: GET route for first and last names of all women
 app.get('/api/data/women-names', (req, res) => {
   const womenNames = data
     .filter(item => item.gender === 'Female')
@@ -35,7 +30,6 @@ app.get('/api/data/women-names', (req, res) => {
   res.json(womenNames);
 });
 
-// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
